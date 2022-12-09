1258cfd2c67a83e10808501dfb42ec3987433b43
rachelduret <chuncheung.ku@gmail.com>
2022-12-09 21:17:08 +0100

    add profile

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 function Header({}: Props) {
 	return (
-		<header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center bg-black ">
+		<header className="sticky top-0 p-5 flex items-start justify-between max-w-8xl mx-auto z-20 xl:items-center">
 			<motion.div
 				initial={{
 					x: -500,
@@ -43,6 +43,30 @@ function Header({}: Props) {
 					fgColor="gray"
 					bgColor="transparent"
 				/>
+			</motion.div>
+
+			<motion.div
+				initial={{
+					x: 500,
+					opacity: 0,
+					scale: 0.5,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 2,
+				}}
+				className="flex flex-row items-center">
+				<SocialIcon
+					className="cursor-pointer"
+					network="email"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+				<p>Contact me</p>
 			</motion.div>
 		</header>
 	);
--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import BackgroundCircles from './BackgroundCircles';
+import Link from 'next/link';
 
 type Props = {};
 
 function Profile({}: Props) {
-	return <div>Profile</div>;
+	return (
+		<div className="h-screen flex flex-col space-y-8 items-center justify-center ">
+			<BackgroundCircles />
+			<h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+				Developpeuse Full Stack
+			</h2>
+			<h1 className="text-5xl lg:text-6xl font-semibold px-10">Rachel DURET</h1>
+
+			<div className="pt-5">
+				<Link href="#about">
+					<button className="profileButton cursor-pointer hover:border-[#F7AB0A]/40 hover: text-[#F7AB0A]/40">
+						About
+					</button>
+				</Link>
+				<Link href="#expriences">
+					<button className="profileButton hover:border-[#F7AB0A]/40 hover: text-[#F7AB0A]/40">
+						experiences
+					</button>
+				</Link>
+				<Link href="#skills">
+					<button className="profileButton hover:border-[#F7AB0A]/40 hover: text-[#F7AB0A]/40">
+						skills
+					</button>
+				</Link>
+				<Link href="#projects">
+					<button className="profileButton hover:border-[#F7AB0A]/40 hover: text-[#F7AB0A]/40">
+						projects
+					</button>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Profile;
