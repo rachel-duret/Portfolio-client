e1a314b5eeb253c603cb1b3fa551144b005a2bed
rachelduret <chuncheung.ku@gmail.com>
2022-12-09 11:56:55 +0100

    add header

---
+++ components/Header.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion';
+
+type Props = {};
+
+function Header({}: Props) {
+	return (
+		<header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center bg-black ">
+			<motion.div
+				initial={{
+					x: -500,
+					opacity: 0,
+					scale: 0.5,
+				}}
+				animate={{
+					x: 0,
+					opacity: 1,
+					scale: 1,
+				}}
+				transition={{
+					duration: 2,
+				}}
+				className="flex flex-row items-center">
+				<SocialIcon
+					url="https://github.com/rachel-duret"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://www.linkedin.com/in/rachelduret"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					url="https://twitter.com/ChuncheungDuret"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+				<SocialIcon
+					className="cursor-pointer"
+					network="email"
+					fgColor="gray"
+					bgColor="transparent"
+				/>
+			</motion.div>
+		</header>
+	);
+}
+
+export default Header;
